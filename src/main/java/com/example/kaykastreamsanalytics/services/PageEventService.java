ce1ea3e094fe54a223d50a7c4f095c36c233a444
Ahmed kharbouch <ahmedsturridge@hotmail.com>
2023-01-13 23:26:26 +0100

    seconde commit

--- src/main/java/com/example/kaykastreamsanalytics/services/PageEventService.java
+++ src/main/java/com/example/kaykastreamsanalytics/services/PageEventService.java
@@ -1,9 +1,16 @@
 package com.example.kaykastreamsanalytics.services;
 
 import com.example.kaykastreamsanalytics.entities.PageEvent;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
@@ -37,6 +44,21 @@ public class PageEventService {
         return (input) -> {
             input.setUser_name(input.getUser_name().toUpperCase()+"-MODIFIED");
             return input;
+        };
+    }
+
+    @Bean
+    public Function<KStream<String,PageEvent>,KStream<String,Long>> kStreamFunction(){
+        return (input) -> {
+            return input
+                    .filter((k,v)->v.getDuration_event()>1000)
+                    .map((k,v)->new KeyValue<>(v.getName_page(),v.getDuration_event()))
+                    //.groupByKey(Grouped.keySerde(Serdes.String()))
+                    .groupBy((k,v)->k,Grouped.with(Serdes.String(),Serdes.Long()))
+                    .windowedBy(TimeWindows.of(Duration.ofMillis(5000)))
+                    .count(Materialized.as("page-count"))
+                    .toStream()
+                    .map((k,v)->new KeyValue<>("=>"+k.window().startTime()+k.window().endTime()+k.key(),v));
         };
     }
 }
